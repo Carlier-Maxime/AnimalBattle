d8b8476bde2690170fcad2858418e72cf56bfa91
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-01-28 08:56:17 +0100

    adapte gameView (Version 0.0.5 - snapshot 22w04g)

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -28,29 +28,30 @@ public class GameView extends JFrame implements View{
         JPanel overlay = new JPanel();
         overlay.setLayout(new OverlayLayout(overlay));
         add(overlay, constraints);
-        //Preparation GamePanel
+
+        GamePanel gamePanel = new GamePanel();
+        //Preparation zone GamePanel
         JPanel hbox = new JPanel();
         hbox.setLayout(new GridBagLayout());
-        GridBagConstraints c = Utils.initConstraints();
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
         c.insets = new Insets(-1,-1,-1,-1);
         Dimension dim = getToolkit().getScreenSize();
         c.weightx = ((dim.width-dim.height)/2.0)/dim.height;
+        c.weighty = 1;
         c.gridx = 0;
         JPanel leftPan = new JPanel();
         leftPan.setBackground(Color.BLACK);
         hbox.add(leftPan, c);
         c.gridx = 2;
-        JPanel rightPan = new JPanel();
-        rightPan.setBackground(Color.BLACK);
-        hbox.add(rightPan, c);
+        hbox.add(new InfoCharacter(((Level) gamePanel.getModel()).getCharacter()), c);
         c.gridx = 1;
         c.weightx = 1;
         overlay.add(hbox, constraints);
-        //GamePanel
-        GamePanel gamePanel = new GamePanel();
+
+        //add GamePanel
         hbox.add(gamePanel, c);
         this.controller.setControllerChild(gamePanel.getController());
-        hbox.add(new InfoCharacter(((Level) gamePanel.getModel()).getCharacter()), c);
 
         //set visibile and focus
         setVisible(true);
